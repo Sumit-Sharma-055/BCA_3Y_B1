35895a3e7bbd5a00a9010efc6f84a58a8846826b
Sumit-Sharma-055 <xumit97575235@gmail.com>
2024-08-20 15:26:54 +0530

    edited

--- PROJ1/script.js
+++ PROJ1/script.js
@@ -3,12 +3,16 @@ let student = {
     name: "Sumit",
     age: 20,
     isWorking: false,
-    src:""
+    src:"https://cdn.vectorstock.com/i/1000v/98/56/cute-little-boy-cartoon-vector-21539856.jpg"
 }
 let {id, name, age, isWorking, src} = student
 let info = `
-    <h1> Welcome</h1>
-    <img src=${src}>
+    <h1> IT'S MINE PORTFOLIO </h1>
+    <img src="${src}" alt="no image" height="100px" width="100px" >
+    <h2> id: ${id} </h2>
+    <h2> name: ${name} </h2>
+    <h3> age: ${age} </h3>
+    <h3> Working/student:${isWorking?"working profrssion":"student"}</h3>
 `
 
 document.write(info)
